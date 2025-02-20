cb2195414ddcbf80afadb26c6910e34456acebee
Evgeny Kniazev <eugene.kniazev@gmail.com>
2025-02-20 22:07:12 +0100

    upd

---
+++ web/app/routes/video.tsx
@@ -0,0 +1,25 @@
+import { createFileRoute } from "@tanstack/react-router"
+
+const VideoCapture = () => {
+  const handleVideoCapture = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      console.log("Captured video file:", file)
+      // You can add further processing of the captured video file here
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="video/*;capture=camcorder"
+        onChange={handleVideoCapture}
+      />
+    </div>
+  )
+}
+
+export const Route = createFileRoute("/")({
+  component: VideoCapture,
+})
